4baf9db97cace97a7b6232383c59e6b27c5572c8
ilg-ul <ilg-ul@73bffd24-bf21-0410-921b-c1c13da472a1>
2009-04-18 08:30:21 +0000

    cleanups
    git-svn-id: svn://localhost/gnuarmeclipse/trunk@104 73bffd24-bf21-0410-921b-c1c13da472a1

--- Eclipse/org.eclipse.cdt.cross.arm.gnu/src/org/eclipse/cdt/cross/arm/gnu/sourcery/windows/ConfigurationEnvironmentSupplier.java
+++ Eclipse/org.eclipse.cdt.cross.arm.gnu/src/org/eclipse/cdt/cross/arm/gnu/sourcery/windows/ConfigurationEnvironmentSupplier.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2005, 2007 Intel Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- * Intel Corporation - Initial API and implementation
- *******************************************************************************/
 package org.eclipse.cdt.cross.arm.gnu.sourcery.windows;
 
 import org.eclipse.cdt.managedbuilder.core.IConfiguration;
@@ -22,7 +12,6 @@ public class ConfigurationEnvironmentSupplier implements
 	static final String VARNAME_PATH = "PATH"; //$NON-NLS-1$
 	static final String DELIMITER_UNIX = ":"; //$NON-NLS-1$
 	static final String PROPERTY_DELIMITER = "path.separator"; //$NON-NLS-1$
-	//static final String PROPERTY_OSNAME = "os.name"; //$NON-NLS-1$
 
 	public IBuildEnvironmentVariable getVariable(String variableName,
 			IConfiguration configuration, IEnvironmentVariableProvider provider) {
