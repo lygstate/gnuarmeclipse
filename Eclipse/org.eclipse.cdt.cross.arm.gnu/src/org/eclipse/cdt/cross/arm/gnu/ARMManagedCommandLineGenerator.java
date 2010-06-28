139aaf6aded4cd064041984843081f9356cb1cc7
ilg-ul <ilg-ul@73bffd24-bf21-0410-921b-c1c13da472a1>
2010-06-28 18:48:07 +0000

    generateCommandLineInfo() bug fixed (parent of Tool may be ResourceConfiguration)
    git-svn-id: svn://localhost/gnuarmeclipse/trunk@163 73bffd24-bf21-0410-921b-c1c13da472a1

--- Eclipse/org.eclipse.cdt.cross.arm.gnu/src/org/eclipse/cdt/cross/arm/gnu/ARMManagedCommandLineGenerator.java
+++ Eclipse/org.eclipse.cdt.cross.arm.gnu/src/org/eclipse/cdt/cross/arm/gnu/ARMManagedCommandLineGenerator.java
@@ -46,138 +46,143 @@ public class ARMManagedCommandLineGenerator extends ManagedCommandLineGenerator 
 		oList.addAll(((java.util.Collection<String>) (Arrays
 				.asList(((asFlags))))));
 
-		IToolChain oToolChain = (IToolChain) oTool.getParent();
-		// IConfiguration iconfiguration = itoolchain.getParent();
-		IOption aoOptions[] = oToolChain.getOptions();
+		Object oParent = oTool.getParent();
+		if (oParent instanceof IToolChain) {
+			IToolChain oToolChain = (IToolChain) oParent;
+			// IConfiguration iconfiguration = itoolchain.getParent();
+			IOption aoOptions[] = oToolChain.getOptions();
 
-		String sProcessor;
-		sProcessor = null;
+			String sProcessor;
+			sProcessor = null;
 
-		String sThumb;
-		sThumb = null;
+			String sThumb;
+			sThumb = null;
 
-		String sThumbInterwork;
-		sThumbInterwork = null;
+			String sThumbInterwork;
+			sThumbInterwork = null;
 
-		String sDebugLevel;
-		sDebugLevel = null;
+			String sDebugLevel;
+			sDebugLevel = null;
 
-		String sDebugFormat;
-		sDebugFormat = null;
+			String sDebugFormat;
+			sDebugFormat = null;
 
-		String sDebugOther;
-		sDebugOther = null;
+			String sDebugOther;
+			sDebugOther = null;
 
-		String sDebugProf;
-		sDebugProf = null;
+			String sDebugProf;
+			sDebugProf = null;
 
-		String sDebugGProf;
-		sDebugGProf = null;
+			String sDebugGProf;
+			sDebugGProf = null;
 
-		for (int i = 0; i < aoOptions.length; ++i) {
-			IOption oOption;
-			oOption = aoOptions[i];
+			for (int i = 0; i < aoOptions.length; ++i) {
+				IOption oOption;
+				oOption = aoOptions[i];
 
-			String sID;
-			sID = oOption.getId();
+				String sID;
+				sID = oOption.getId();
 
-			Object oValue;
-			oValue = oOption.getValue();
+				Object oValue;
+				oValue = oOption.getValue();
 
-			String sCommand;
-			sCommand = oOption.getCommand();
+				String sCommand;
+				sCommand = oOption.getCommand();
 
-			if (oValue instanceof String) {
-				String sVal;
-				try {
-					sVal = oOption.getStringValue();
-				} catch (BuildException e) {
-					sVal = null;
+				if (oValue instanceof String) {
+					String sVal;
+					try {
+						sVal = oOption.getStringValue();
+					} catch (BuildException e) {
+						sVal = null;
+					}
+
+					String sEnumCommand;
+					try {
+						sEnumCommand = oOption.getEnumCommand(sVal);
+					} catch (BuildException e1) {
+						sEnumCommand = null;
+					}
+
+					if (DEBUG_LOCAL)
+						System.out.println(oOption.getName() + " " + sID + " "
+								+ sVal + " " + sCommand + " " + sEnumCommand);
+
+					if (sID.endsWith(OPTION_SUFIX_PROCESSOR)
+							|| sID.indexOf(OPTION_SUFIX_PROCESSOR + ".") > 0) {
+						sProcessor = sEnumCommand;
+					} else if (sID.endsWith(OPTION_SUFIX_DEBUGGING_LEVEL)
+							|| sID.indexOf(OPTION_SUFIX_DEBUGGING_LEVEL + ".") > 0) {
+						sDebugLevel = sEnumCommand;
+					} else if (sID.endsWith(OPTION_SUFIX_DEBUGGING_FORMAT)
+							|| sID.indexOf(OPTION_SUFIX_DEBUGGING_FORMAT + ".") > 0) {
+						sDebugFormat = sEnumCommand;
+					} else if (sID.endsWith(OPTION_SUFIX_DEBUGGING_OTHER)
+							|| sID.indexOf(OPTION_SUFIX_DEBUGGING_OTHER + ".") > 0) {
+						sDebugOther = sVal;
+					}
+				} else if (oValue instanceof Boolean) {
+					boolean bVal;
+					try {
+						bVal = oOption.getBooleanValue();
+					} catch (BuildException e) {
+						bVal = false;
+					}
+
+					if (DEBUG_LOCAL)
+						System.out.println(oOption.getName() + " " + sID + " "
+								+ bVal + " " + sCommand);
+
+					if (sID.endsWith(OPTION_SUFIX_THUMB)
+							|| sID.indexOf(OPTION_SUFIX_THUMB + ".") > 0) {
+						if (bVal)
+							sThumb = sCommand;
+					} else if (sID.endsWith(OPTION_SUFIX_THUMB_INTERWORK)
+							|| sID.indexOf(OPTION_SUFIX_THUMB_INTERWORK + ".") > 0) {
+						if (bVal)
+							sThumbInterwork = sCommand;
+					} else if (sID.endsWith(OPTION_SUFIX_DEBUGGING_PROF)
+							|| sID.indexOf(OPTION_SUFIX_DEBUGGING_PROF + ".") > 0) {
+						if (bVal)
+							sDebugProf = sCommand;
+					} else if (sID.endsWith(OPTION_SUFIX_DEBUGGING_GPROF)
+							|| sID.indexOf(OPTION_SUFIX_DEBUGGING_GPROF + ".") > 0) {
+						if (bVal)
+							sDebugGProf = sCommand;
+					}
+				} else {
+					if (DEBUG_LOCAL)
+						System.out.println(oOption.getName() + " " + sID + " "
+								+ oValue + " " + sCommand);
 				}
-
-				String sEnumCommand;
-				try {
-					sEnumCommand = oOption.getEnumCommand(sVal);
-				} catch (BuildException e1) {
-					sEnumCommand = null;
-				}
-
-				if (DEBUG_LOCAL)
-					System.out.println(oOption.getName() + " " + sID + " "
-							+ sVal + " " + sCommand + " " + sEnumCommand);
-
-				if (sID.endsWith(OPTION_SUFIX_PROCESSOR)
-						|| sID.indexOf(OPTION_SUFIX_PROCESSOR + ".") > 0) {
-					sProcessor = sEnumCommand;
-				} else if (sID.endsWith(OPTION_SUFIX_DEBUGGING_LEVEL)
-						|| sID.indexOf(OPTION_SUFIX_DEBUGGING_LEVEL + ".") > 0) {
-					sDebugLevel = sEnumCommand;
-				} else if (sID.endsWith(OPTION_SUFIX_DEBUGGING_FORMAT)
-						|| sID.indexOf(OPTION_SUFIX_DEBUGGING_FORMAT + ".") > 0) {
-					sDebugFormat = sEnumCommand;
-				} else if (sID.endsWith(OPTION_SUFIX_DEBUGGING_OTHER)
-						|| sID.indexOf(OPTION_SUFIX_DEBUGGING_OTHER + ".") > 0) {
-					sDebugOther = sVal;
-				}
-			} else if (oValue instanceof Boolean) {
-				boolean bVal;
-				try {
-					bVal = oOption.getBooleanValue();
-				} catch (BuildException e) {
-					bVal = false;
-				}
-
-				if (DEBUG_LOCAL)
-					System.out.println(oOption.getName() + " " + sID + " "
-							+ bVal + " " + sCommand);
-
-				if (sID.endsWith(OPTION_SUFIX_THUMB)
-						|| sID.indexOf(OPTION_SUFIX_THUMB + ".") > 0) {
-					if (bVal)
-						sThumb = sCommand;
-				} else if (sID.endsWith(OPTION_SUFIX_THUMB_INTERWORK)
-						|| sID.indexOf(OPTION_SUFIX_THUMB_INTERWORK + ".") > 0) {
-					if (bVal)
-						sThumbInterwork = sCommand;
-				} else if (sID.endsWith(OPTION_SUFIX_DEBUGGING_PROF)
-						|| sID.indexOf(OPTION_SUFIX_DEBUGGING_PROF + ".") > 0) {
-					if (bVal)
-						sDebugProf = sCommand;
-				} else if (sID.endsWith(OPTION_SUFIX_DEBUGGING_GPROF)
-						|| sID.indexOf(OPTION_SUFIX_DEBUGGING_GPROF + ".") > 0) {
-					if (bVal)
-						sDebugGProf = sCommand;
-				}
-			} else {
-				if (DEBUG_LOCAL)
-					System.out.println(oOption.getName() + " " + sID + " "
-							+ oValue + " " + sCommand);
 			}
+
+			if (DEBUG_LOCAL)
+				System.out.println(sProcessor + " " + sThumb + " "
+						+ sThumbInterwork + " " + sDebugLevel + " "
+						+ sDebugFormat + " " + sDebugOther + " " + sDebugProf
+						+ " " + sDebugGProf);
+
+			if (sProcessor != null && sProcessor.length() > 0)
+				oList.add(sProcessor);
+			if (sThumb != null && sThumb.length() > 0)
+				oList.add(sThumb);
+			if (sThumbInterwork != null && sThumbInterwork.length() > 0)
+				oList.add(sThumbInterwork);
+			if (sDebugLevel != null && sDebugLevel.length() > 0) {
+				oList.add(sDebugLevel);
+
+				if (sDebugFormat != null && sDebugFormat.length() > 0)
+					oList.add(sDebugFormat);
+			}
+			if (sDebugOther != null && sDebugOther.length() > 0)
+				oList.add(sDebugOther);
+			if (sDebugProf != null && sDebugProf.length() > 0)
+				oList.add(sDebugProf);
+			if (sDebugGProf != null && sDebugGProf.length() > 0)
+				oList.add(sDebugGProf);
+
 		}
-
-		if (DEBUG_LOCAL)
-			System.out.println(sProcessor + " " + sThumb + " "
-					+ sThumbInterwork + " " + sDebugLevel + " " + sDebugFormat
-					+ " " + sDebugOther + " " + sDebugProf + " " + sDebugGProf);
-
-		if (sProcessor != null && sProcessor.length() > 0)
-			oList.add(sProcessor);
-		if (sThumb != null && sThumb.length() > 0)
-			oList.add(sThumb);
-		if (sThumbInterwork != null && sThumbInterwork.length() > 0)
-			oList.add(sThumbInterwork);
-		if (sDebugLevel != null && sDebugLevel.length() > 0) {
-			oList.add(sDebugLevel);
-
-			if (sDebugFormat != null && sDebugFormat.length() > 0)
-				oList.add(sDebugFormat);
-		}
-		if (sDebugOther != null && sDebugOther.length() > 0)
-			oList.add(sDebugOther);
-		if (sDebugProf != null && sDebugProf.length() > 0)
-			oList.add(sDebugProf);
-		if (sDebugGProf != null && sDebugGProf.length() > 0)
-			oList.add(sDebugGProf);
 
 		return super.generateCommandLineInfo(oTool, sCommandName, oList
 				.toArray(new String[0]), sOutputFlag, sOutputPrefix,
