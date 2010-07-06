9a31fce6cc1a41d261c2863feb7a6176d686983b
ilg-ul <ilg-ul@73bffd24-bf21-0410-921b-c1c13da472a1>
2010-07-06 15:25:36 +0000

    common compiler name for all Sourcery cases
    git-svn-id: svn://localhost/gnuarmeclipse/trunk@167 73bffd24-bf21-0410-921b-c1c13da472a1

---
+++ Eclipse/org.eclipse.cdt.cross.arm.gnu/src/org/eclipse/cdt/cross/arm/gnu/sourcery/IsToolChainSupported.java
@@ -0,0 +1,10 @@
+package org.eclipse.cdt.cross.arm.gnu.sourcery;
+
+public abstract class IsToolChainSupported extends
+		org.eclipse.cdt.cross.arm.gnu.common.IsToolChainSupported {
+	
+	public String getCompilerName() {
+		return "arm-none-eabi-gcc";
+	}
+
+}
