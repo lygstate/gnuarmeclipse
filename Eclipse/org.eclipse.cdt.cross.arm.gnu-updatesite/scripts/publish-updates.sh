da1555747f1331139b8a4e728b83b44d783f623a
ilg-ul <ilg-ul@73bffd24-bf21-0410-921b-c1c13da472a1>
2012-01-24 22:00:12 +0000

    publish script added
    git-svn-id: svn://localhost/gnuarmeclipse/trunk@271 73bffd24-bf21-0410-921b-c1c13da472a1

---
+++ Eclipse/org.eclipse.cdt.cross.arm.gnu-updatesite/scripts/publish-updates.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+# This script publishes the org.eclipse.cdt.cross.arm.gnu-updatesite site
+# to the File Release System (FRS) folder on SourceForge
+
+if [ $# -gt 0 ] && [ "$1" = "test" ]
+then
+  SF_FOLDER="Eclipse/updates-test"
+  shift
+else
+  SF_FOLDER="Eclipse/updates"
+fi
+
+echo "Updating $SF_FOLDER"
+
+if [ $# -gt 0 ] && [ "$1" = "dry" ]
+then
+  DRY="dry"
+  shift
+  echo "Dry run"
+fi
+
+
+SF_USER=ilg-ul
+SF_DESTINATION="$SF_USER,gnuarmeclipse@frs.sourceforge.net:/home/frs/project/g/gn/gnuarmeclipse/$SF_FOLDER"
+SOURCE_LIST="."
+
+RSYNC_OPTS="-vrCt --exclude=scripts --exclude=.*"
+RSYNC_OPTS+=" --delete"
+
+if [ "$DRY" = "dry" ]
+then
+  RSYNC_OPTS+=" -n"
+fi
+
+(cd ..; rsync -e ssh $RSYNC_OPTS $SOURCE_LIST $SF_DESTINATION)
