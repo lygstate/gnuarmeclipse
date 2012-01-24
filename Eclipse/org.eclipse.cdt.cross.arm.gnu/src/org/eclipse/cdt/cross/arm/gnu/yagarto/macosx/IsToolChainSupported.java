ba0b067b85dd7794c189da4ad314004a9d84d08c
ilg-ul <ilg-ul@73bffd24-bf21-0410-921b-c1c13da472a1>
2012-01-24 20:55:20 +0000

    support for Mac OS X Yagarto added
    git-svn-id: svn://localhost/gnuarmeclipse/trunk@270 73bffd24-bf21-0410-921b-c1c13da472a1

---
+++ Eclipse/org.eclipse.cdt.cross.arm.gnu/src/org/eclipse/cdt/cross/arm/gnu/yagarto/macosx/IsToolChainSupported.java
@@ -0,0 +1,26 @@
+package org.eclipse.cdt.cross.arm.gnu.yagarto.macosx;
+
+import org.eclipse.cdt.cross.arm.gnu.Tools;
+import org.eclipse.cdt.cross.arm.gnu.common.IsToolchainData;
+import org.eclipse.cdt.managedbuilder.core.IToolChain;
+import org.osgi.framework.Version;
+
+public class IsToolChainSupported extends
+		org.eclipse.cdt.cross.arm.gnu.yagarto.IsToolChainSupported {
+
+	static IsToolchainData ms_oData = null;
+
+	public boolean isSupported(IToolChain oToolChain,
+			Version oVersion, String sInstance) {
+		
+		if (ms_oData == null)
+			ms_oData = new IsToolchainData();
+		
+		return isSupportedImpl(oToolChain, oVersion, sInstance,
+				ms_oData);
+	}
+	
+	public String getPlatform() {
+		return Tools.PROPERTY_OS_VALUE_MACOSX;
+	}
+}
