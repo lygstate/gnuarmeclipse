fb94191212ebef26a7bea1a278586d7dcb9e7cb1
ilg-ul <ilg-ul@73bffd24-bf21-0410-921b-c1c13da472a1>
2012-02-17 16:37:07 +0000

    update to use Eclipse update site
    git-svn-id: svn://localhost/gnuarmeclipse/trunk@314 73bffd24-bf21-0410-921b-c1c13da472a1

--- Eclipse/org.eclipse.cdt.cross.arm.gnu-updatesite/scripts/prepare-archive.sh
+++ Eclipse/org.eclipse.cdt.cross.arm.gnu-updatesite/scripts/prepare-archive.sh
@@ -1,7 +1,24 @@
 #! /bin/bash
 
-ARCHIVE_NAME=$(grep "org.eclipse.cdt.cross.arm.gnu" ../site.xml | sed 's/.*features\/\(org.eclipse.cdt.cross.arm.gnu.*\).jar.*/\1/')
+# Script to run on Mac OS X and pack the FRS distribution archive using the
+# version published in the Eclipse/updates site
 
-LOCAL_SOURCES="site.xml index.html content.jar artifacts.jar plugins features"
-(cd ..; zip -v scripts/$ARCHIVE_NAME.zip $LOCAL_SOURCES)
+# It creates a temporary folder to download the update site
 
+TMP_FOLDER=/tmp/gnuarmeclipse-publish
+echo "Creating temporary folder $TMP_FOLDER"
+mkdir -p $TMP_FOLDER
+cd $TMP_FOLDER
+
+rm -rf updates
+echo "Fetching Eclipse updates site"
+rsync -vrCt -e ssh ilg-ul,gnuarmeclipse@frs.sourceforge.net:/home/frs/project/g/gn/gnuarmeclipse/Eclipse/updates .
+
+ARCHIVE_NAME=$(grep "org.eclipse.cdt.cross.arm.gnu" updates/site.xml | sed 's/.*features\/\(org.eclipse.cdt.cross.arm.gnu.*\).jar.*/\1/')
+rm -f $ARCHIVE_NAME.zip
+
+echo "Packing archive"
+LOCAL_SOURCES="site.xml index.html content.jar artifacts.jar plugins/*.jar features/*.jar"
+(cd updates; zip -v ../$ARCHIVE_NAME.zip $LOCAL_SOURCES)
+
+ls -l $ARCHIVE_NAME.zip
